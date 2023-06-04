4cf43d7a57350335cd72c766649dded9bf1a6dd6
th1enlm02 <21521460@gm.uit.edu.vn>
2023-06-04 12:09:05 +0700

    Đề tài 04 - Nhóm 04

---
+++ AppChat/Message.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace AppChat
+{
+    partial class Message
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Message));
+            this.bunifuElipse1 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.bunifuGradientPanel1 = new Bunifu.UI.WinForms.BunifuGradientPanel();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.bunifuLabelText = new Bunifu.UI.WinForms.BunifuLabel();
+            this.labelCaption = new System.Windows.Forms.Label();
+            this.bunifuElipse2 = new Bunifu.Framework.UI.BunifuElipse(this.components);
+            this.bunifuGradientPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bunifuElipse1
+            // 
+            this.bunifuElipse1.ElipseRadius = 50;
+            this.bunifuElipse1.TargetControl = this;
+            // 
+            // bunifuGradientPanel1
+            // 
+            this.bunifuGradientPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.bunifuGradientPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("bunifuGradientPanel1.BackgroundImage")));
+            this.bunifuGradientPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.bunifuGradientPanel1.BorderRadius = 1;
+            this.bunifuGradientPanel1.Controls.Add(this.buttonOK);
+            this.bunifuGradientPanel1.Controls.Add(this.bunifuLabelText);
+            this.bunifuGradientPanel1.Controls.Add(this.labelCaption);
+            this.bunifuGradientPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.bunifuGradientPanel1.GradientBottomLeft = System.Drawing.Color.FromArgb(((int)(((byte)(236)))), ((int)(((byte)(92)))), ((int)(((byte)(188)))));
+            this.bunifuGradientPanel1.GradientBottomRight = System.Drawing.Color.LightSkyBlue;
+            this.bunifuGradientPanel1.GradientTopLeft = System.Drawing.Color.LightSkyBlue;
+            this.bunifuGradientPanel1.GradientTopRight = System.Drawing.Color.FromArgb(((int)(((byte)(198)))), ((int)(((byte)(60)))), ((int)(((byte)(212)))));
+            this.bunifuGradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.bunifuGradientPanel1.Name = "bunifuGradientPanel1";
+            this.bunifuGradientPanel1.Quality = 10;
+            this.bunifuGradientPanel1.Size = new System.Drawing.Size(398, 226);
+            this.bunifuGradientPanel1.TabIndex = 3;
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.buttonOK.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonOK.Location = new System.Drawing.Point(151, 165);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(86, 34);
+            this.buttonOK.TabIndex = 2;
+            this.buttonOK.Text = "OK";
+            this.buttonOK.UseVisualStyleBackColor = false;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // bunifuLabelText
+            // 
+            this.bunifuLabelText.AllowParentOverrides = false;
+            this.bunifuLabelText.AutoEllipsis = false;
+            this.bunifuLabelText.AutoSize = false;
+            this.bunifuLabelText.Cursor = System.Windows.Forms.Cursors.Default;
+            this.bunifuLabelText.CursorType = System.Windows.Forms.Cursors.Default;
+            this.bunifuLabelText.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuLabelText.Location = new System.Drawing.Point(31, 68);
+            this.bunifuLabelText.Name = "bunifuLabelText";
+            this.bunifuLabelText.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.bunifuLabelText.Size = new System.Drawing.Size(332, 74);
+            this.bunifuLabelText.TabIndex = 1;
+            this.bunifuLabelText.Text = "Text";
+            this.bunifuLabelText.TextAlignment = System.Drawing.ContentAlignment.MiddleCenter;
+            this.bunifuLabelText.TextFormat = Bunifu.UI.WinForms.BunifuLabel.TextFormattingOptions.Default;
+            // 
+            // labelCaption
+            // 
+            this.labelCaption.AutoSize = true;
+            this.labelCaption.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCaption.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labelCaption.Location = new System.Drawing.Point(12, 9);
+            this.labelCaption.Name = "labelCaption";
+            this.labelCaption.Size = new System.Drawing.Size(81, 24);
+            this.labelCaption.TabIndex = 0;
+            this.labelCaption.Text = "Caption";
+            // 
+            // bunifuElipse2
+            // 
+            this.bunifuElipse2.ElipseRadius = 20;
+            this.bunifuElipse2.TargetControl = this.buttonOK;
+            // 
+            // Message
+            // 
+            this.AcceptButton = this.buttonOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(398, 226);
+            this.Controls.Add(this.bunifuGradientPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Message";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "MassageLogin";
+            this.bunifuGradientPanel1.ResumeLayout(false);
+            this.bunifuGradientPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse1;
+        private Bunifu.UI.WinForms.BunifuGradientPanel bunifuGradientPanel1;
+        private System.Windows.Forms.Button buttonOK;
+        private Bunifu.Framework.UI.BunifuElipse bunifuElipse2;
+        internal Bunifu.UI.WinForms.BunifuLabel bunifuLabelText;
+        internal System.Windows.Forms.Label labelCaption;
+    }
+}
